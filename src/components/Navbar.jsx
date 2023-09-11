9042bf43566b73074eee904919eb543034a8981d
jazz <jazxbx@gmail.com>
2023-09-11 14:57:17 -0700

    added css to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="nav-wrapper">
+      <Link to={"/"}>Home</Link>
+      <Link to={"/"}>Login</Link>
+      <Link to={"/"}>Register</Link>
+    </nav>
+  );
 }
 
 export default Navbar;
