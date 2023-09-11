cc214e015dfd6609cef0518ffeee6a9a5bc2c784
jazz <jazxbx@gmail.com>
2023-09-11 14:53:50 -0700

    api req added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
+import { API } from "./API";
+
 function App() {
-  return <></>;
+  const [paintings, setPaintings] = useState([]);
+
+  async function fetchPaintings() {
+    try {
+      const res = await fetch(`${API}`);
+      const data = await res.json();
+      setPaintings(data.records);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPaintings();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Outlet context={{ paintings }} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Painting.jsx
@@ -0,0 +1,5 @@
+function Painting() {
+  return <div>Painting</div>;
+}
+
+export default Painting;
