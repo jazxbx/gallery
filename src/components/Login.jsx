f3c85084e718b76547d14124d7a3a159b6c1c720
jazz <jazxbx@gmail.com>
2023-09-12 13:17:41 -0700

    inital form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,14 @@
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="form-container">
+      <form className="form" action="">
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" />
+        <input type="text" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
