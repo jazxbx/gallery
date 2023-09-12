f12cce5f893253416724864c2d0432c8b371ef71
jazz <jazxbx@gmail.com>
2023-09-12 11:33:22 -0700

    added jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,23 @@
+import { useOutletContext, Link } from "react-router-dom";
+
 function Gallery() {
-  return <div>Gallery</div>;
+  const { paintings } = useOutletContext();
+  return (
+    <div>
+      {paintings.map((painting) => (
+        <div key={painting.id}>
+          <Link to={`/painting/${painting.id}`}>
+            <div>{painting.title}</div>
+            {painting.images.length > 0 ? (
+              <img src={painting.images[0].baseimageurl} alt={painting.title} />
+            ) : (
+              <p>No image found</p>
+            )}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Gallery;
