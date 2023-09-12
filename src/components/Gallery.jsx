82a94ed3e4eb6c284ec0ca43b6e7a88d7c16ade7
jazz <jazxbx@gmail.com>
2023-09-12 11:47:43 -0700

    inital css for gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,10 +2,13 @@ import { useOutletContext, Link } from "react-router-dom";
 
 function Gallery() {
   const { paintings } = useOutletContext();
+
+  // filter first??
+
   return (
-    <div>
+    <div className="gallery-container">
       {paintings.map((painting) => (
-        <div key={painting.id}>
+        <div className="painting-container" key={painting.id}>
           <Link to={`/painting/${painting.id}`}>
             {painting.images.length > 0 && (
               <img src={painting.images[0].baseimageurl} alt={painting.title} />
