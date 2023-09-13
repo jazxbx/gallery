f1565d6943e21a85743ea3c08d67fd2181bfbc1a
jazz <jazxbx@gmail.com>
2023-09-12 17:46:45 -0700

    added register fn

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+import { useOutletContext, useNavigate } from "react-router-dom";
+
 function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUser } = useOutletContext();
+  const navigate = useNavigate();
+
+  function handleLogin(e) {
+    e.preventDefault();
+    setUser({ id: 1, username: username ? username : "Monkey D Luffy" });
+    navigate("/");
+  }
+
   return (
     <div className="form-container">
-      <form className="form" action="">
-        <h1>Register</h1>
-        <input type="text" placeholder="Username" />
-        <input type="text" placeholder="Password" />
-        <button>Login</button>
+      <form className="form" onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Register</button>
       </form>
     </div>
   );
