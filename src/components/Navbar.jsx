36c6c0782469533be9f61b9ac57248a2462a058f
jazz <jazxbx@gmail.com>
2023-09-12 17:29:58 -0700

    added conditionals

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ user, setUser }) {
+  const handleLogout = () => {
+    setUser({});
+  };
+
+  const isUserLoggedIn = user && user.username;
+
   return (
-    <nav className="nav-wrapper">
-      <Link to={"/"}>Home</Link>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/register"}>Register</Link>
+    <nav>
+      <ul className="nav-wrapper">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          {isUserLoggedIn ? (
+            <p>Welcome {user.username}</p>
+          ) : (
+            <Link to={"/login"}>Login</Link>
+          )}
+        </li>
+        <li>
+          {isUserLoggedIn && (
+            <Link to="/" onClick={handleLogout}>
+              Logout
+            </Link>
+          )}
+        </li>
+        <li>{!isUserLoggedIn && <Link to={"/register"}>Register</Link>}</li>
+      </ul>
     </nav>
   );
 }
